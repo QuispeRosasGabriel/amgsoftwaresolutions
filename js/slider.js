54154629f62437f5c1c01246b3c008d8e44925f3
GabrielQuispeRosas <46933655+QuispeRosasGabriel@users.noreply.github.com>
2024-01-07 20:06:25 -0500

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,24 @@
+const mainSlider = document.getElementById("carouselExampleIndicators");
+const aboutSection = document.getElementById("about");
+const servicesSection = document.getElementById("services");
+const portfolioSection = document.getElementById("portfolio");
+const contactSection = document.getElementById("contact");
+const totalSlides = 26;
+const nodeWrapper = document.getElementById("slide-track");
+let currentIndex = 0;
+
+const generateSlider = () => {
+  for (let index = 1; index <= totalSlides; index++) {
+    const newNode = document.createElement("img");
+    newNode.src = `images/technologies/${index}.svg`;
+    newNode.alt = "";
+    nodeWrapper.appendChild(newNode);
+  }
+};
+
+function scrollToSection(targetSection) {
+  document.getElementById("body-wrapper").scrollTop = targetSection.offsetTop;
+}
+
+// Initialize the slider on page load
+generateSlider();
