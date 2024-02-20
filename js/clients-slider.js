f4dccbb88157cbf88a64cfb5e7137fe6ec88abc7
GabrielQuispeRosas <46933655+QuispeRosasGabriel@users.noreply.github.com>
2024-02-19 20:53:07 -0500

    Add files via upload

---
+++ js/clients-slider.js
@@ -0,0 +1,15 @@
+const clientWapper = document.getElementById("clients-mobile-slider");
+
+
+const generateClientSlider = () => {
+  for (let el = 1; el <= 10; el++) {
+    console.log(el)
+    const newNode = document.createElement("img");
+    newNode.src = `images/technologies/${el}.svg`;
+    newNode.alt = "";
+    clientWapper.appendChild(newNode);
+  }
+};
+
+// Initialize the slider on page load
+window.addEventListener('DOMContentLoaded', () => generateClientSlider());
